956354d3cc5949f0da5152875538283e7c948d01
fhm <fahim618900@gmail.com>
2025-12-15 17:15:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function generate(){
+  let url = document.getElementById("url").value;
+  let app = document.getElementById("app").value;
+  let pkg = document.getElementById("pkg").value;
+
+  if(!url || !app || !pkg){
+    alert("সব ফিল্ড পূরণ করুন");
+    return;
+  }
 
   fetch("http://localhost:3000/build",{
     method:"POST",
     headers:{ "Content-Type":"application/json" },
-    body:JSON.stringify({
-      url: document.getElementById("url").value,
-      appName: document.getElementById("app").value,
-      packageName: document.getElementById("pkg").value
-    })
+    body: JSON.stringify({url:url, appName:app, packageName:pkg})
   })
-  .then(res=>res.json())
-  .then(data=>{
+  .then(res => res.json())
+  .then(data => {
     if(data.download){
-      window.location.href = "http://localhost:3000" + data.download;
+      window.location.href = "http://localhost:3000"+data.download;
     }else{
       alert(JSON.stringify(data));
     }
-  });
+  })
+  .catch(err => alert("Backend চলছে না"));
 }

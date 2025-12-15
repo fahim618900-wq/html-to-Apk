c77853e4cd2b6de50804aa17a2ae577cda62178d
fhm <fahim618900@gmail.com>
2025-12-15 16:34:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 function generate(){
+
+  let url = document.getElementById("url").value;
+  let app = document.getElementById("app").value;
+  let pkg = document.getElementById("pkg").value;
+
+  if(!url || !app || !pkg){
+    alert("সব ফিল্ড পূরণ করুন");
+    return;
+  }
+
   fetch("http://localhost:3000/build",{
     method:"POST",
-    headers:{ "Content-Type":"application/json" },
-    body:JSON.stringify({
-      url: document.getElementById("url").value,
-      appName: document.getElementById("app").value,
-      packageName: document.getElementById("pkg").value
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      url: url,
+      appName: app,
+      packageName: pkg
     })
   })
   .then(res => res.json())
-  .then(data => alert(JSON.stringify(data)))
-  .catch(err => alert("Backend not running"));
+  .then(data => {
+    alert(JSON.stringify(data));
+  })
+  .catch(err => {
+    alert("Backend চলছে না");
+  });
 }
